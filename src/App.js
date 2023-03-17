85f74d59f2b0f6b20dd65a7a511238a7c24c5e19
Vlad Khrystianchuk <ladislaokh@gmail.com>
2023-03-17 23:10:03 +0200

    maim

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { heroesFetching, heroesFetched, heroesFetchingError } from './redux/actions/actions';
+import { useDispatch, useSelector } from 'react-redux';
 
-function App() {
+const App = () => {
+  const {heroesLoadingStatus} = useSelector(state => state);
+  const dispatch = useDispatch();
+  console.log(heroesLoadingStatus)
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <button onClick={() => dispatch(heroesFetching())}>HEROES_FETCHING</button>
+      <br />
+      <button onClick={() => dispatch(heroesFetched())}>HEROES_FETCHED</button>
+      <br />
+      <button onClick={() => dispatch(heroesFetchingError())}>HEROES_FETCHING_ERROR</button>
+    </div>
   )
 }
 
---
+++ src/redux/reducers/reducer.js
@@ -0,0 +1,27 @@
+const initialState = {
+  heroesLoadingStatus: 'idle',
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+      case 'HEROES_FETCHING':
+          return {
+              ...state,
+              heroesLoadingStatus: 'loading'
+          }
+      case 'HEROES_FETCHED':
+          return {
+              ...state,
+              heroes: action.payload,
+              heroesLoadingStatus: 'idle'
+          }
+      case 'HEROES_FETCHING_ERROR':
+          return {
+              ...state,
+              heroesLoadingStatus: 'error'
+          }
+      default: return state
+  }
+}
+
+export default reducer;
